8be7c07e2bb13b73549bd1b5e0b32522c4121697
parthshah15111998 <parths.letsnurture@gmail.com>
2022-03-09 19:27:30 +0530

    Done api coroutines and data binding

--- app/src/main/java/com/example/apicoroutines/RetrofitHelper.kt
+++ app/src/main/java/com/example/apicoroutines/RetrofitHelper.kt
@@ -1,15 +1,21 @@
 package com.example.apicoroutines
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    const val BASE_URL="https://quotable.io"
+     const val BASE_URL="https://quotable.io"
+
+    var logger = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    var okHttpClient: OkHttpClient.Builder = OkHttpClient.Builder().addInterceptor(logger)
 
     fun getInstance():Retrofit{
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient.build())
             .build()
     }
 }
